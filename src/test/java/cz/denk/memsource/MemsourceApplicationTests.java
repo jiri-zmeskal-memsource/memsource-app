62a63d93692be0486913d668fd34c87b4168d2bd
Jakub Denk <jakub.denk@tado.com>
2020-06-12 15:58:42 +0200

    initialize spring-boot backend

---
+++ src/test/java/cz/denk/memsource/MemsourceApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.denk.memsource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemsourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
