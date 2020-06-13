1467c99ae1d28940a7475a73b8e5c399090d3529
Jakub Denk <jakub.denk@tado.com>
2020-06-13 14:35:43 +0200

    point all URIs (except the rest-api ones) to angular application

--- src/main/java/cz/denk/memsource/MemsourceApplication.java
+++ src/main/java/cz/denk/memsource/MemsourceApplication.java
@@ -3,18 +3,40 @@ package cz.denk.memsource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
 
 @SpringBootApplication
-public class MemsourceApplication {
+public class MemsourceApplication implements WebMvcConfigurer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MemsourceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MemsourceApplication.class, args);
+    }
 
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource : new ClassPathResource("/static/index.html");
+                    }
+                });
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
